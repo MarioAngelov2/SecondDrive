e37f3b10ce3689787a25c9d6bc3c622adbc0476e
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-22 08:51:27 +0300

    Created getCar controller

---
+++ server/src/controllers/getCars.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const CarsModel = require("../models/Cars");
+const createHttpError = require("http-errors");
+
+const getCars = async (req, res, next) => {
+    try {
+        const cars = await CarsModel.find({}).exec();
+        res.status(200).json(cars);
+    } catch (error) {
+        next(error)
+    }
+}
