357e1b763af9ed59415e77f4ccd27753672ebd16
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-22 09:55:50 +0300

    Create Car component

---
+++ client/src/components/CreateCar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+import CarForm from "../utils/CarForm";
+
+const URL = "http://localhost:5002";
+
+function CreateCar({ onClose, addNewCar }) {
+
+    async function onSubmit(data) {
+        try {
+            const formData = new FormData();
+
+            formData.append("title", data.title);
+            formData.append("price", data.price);
+            formData.append("engine", data.engine);
+            formData.append("description", data.description);
+            formData.append("mileage", data.mileage);
+            formData.append("location", data.location);
+            formData.append("image", data.image);
+
+            const response = await fetch(URL, {
+                method: "POST",
+                body: formData,
+            });
+
+            const newCar = await response.json();
+            addNewCar(newCar);
+
+            onClose();
+        } catch (error) {
+            console.error("Error creating car:", error);
+        }
+    }
+
+    return (
+        <Modal show onHide={onClose} centered>
+            <Modal.Body>
+                <Modal.Title>Добавяне на обява</Modal.Title>
+                <CarForm onSubmit={onSubmit} />
+            </Modal.Body>
+        </Modal>
+    );
+}
+
+export default CreateCar;
