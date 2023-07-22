e868ea82f0c0ba3ccaa02577e86bef1a3440d691
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-22 08:59:29 +0300

    Created getCarById controller

---
+++ server/src/controllers/getCar.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const CarsModel = require("../models/Cars");
+const createHttpError = require("http-errors");
+
+const getCar = async (req, res, next) => {
+    const id = req.params.id;
+
+    try {
+        if (!mongoose.isValidObjectId) {
+            throw createHttpError(400, "Invalid car id");
+        }
+
+        const car = await CarsModel.findById(id).exec();
+
+        if (!car) {
+            throw createHttpError(404, "Car not found");
+        }
+
+        res.status(200).json(car);
+    } catch (error) {
+        next(error)
+    }
+};
