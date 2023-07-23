6c5a972504d7a1731a29182378b1d04192ee9f82
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-23 20:29:56 +0300

    Created register controller

---
+++ server/src/controllers/register.js
@@ -0,0 +1,40 @@
+const UserModel = require("../models/User");
+const createHttpError = require("http-errors");
+const bcrypt = require("bcrypt");
+
+const createUser = async (req, res, next) => {
+    console.log(req.body);
+    const { username, email, password } = req.body;
+
+    try {
+        if (!username || !email || !password) {
+            throw createHttpError(
+                400,
+                "Username, email or password are missing"
+            );
+        }
+
+        const existingUser = await UserModel.findOne({ email: email }).exec();
+
+        if (existingUser) {
+            throw createHttpError(
+                409,
+                "The user already exist. Please log in."
+            );
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await UserModel.create({
+            usernme: username,
+            email: email,
+            password: hashedPassword,
+        });
+
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+module.exports = createUser;
