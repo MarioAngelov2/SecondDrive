b747e5ea0a8e1ca8fde231842ca711f4e46a7f28
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-23 20:30:10 +0300

    Refactor user schema

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,24 +1,24 @@
 const { model, Schema } = require("mongoose");
 
 const UserSchema = new Schema({
-    first_name: {
+    usernme: {
         type: String,
         reqired: true,
-    },
-    last_name: {
-        type: String,
-        reqired: true,
+        unique: true,
     },
     email: {
         type: String,
         reqired: true,
+        select: false,
+        unique: true,
     },
     password: {
         type: String,
         reqired: true,
+        select: false,
     },
 });
 
-const User = new model("User", UserSchema);
+const User = model("User", UserSchema);
 
 module.exports = User;
