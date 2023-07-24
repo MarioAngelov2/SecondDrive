5e22a900adcdcb734320b0e35cec0a4db2d0aabd
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-24 08:58:05 +0300

    Update cars.js

--- server/src/routes/cars.js
+++ server/src/routes/cars.js
@@ -1,15 +1,18 @@
 const { Router } = require("express");
-const upload = require('../utils/uploadFiles')
+const upload = require("../utils/uploadFiles");
 const getCars = require("../controllers/getCars");
 const getCar = require("../controllers/getCar");
 const createCar = require("../controllers/createCar");
 const updateCar = require("../controllers/updateCar");
 const deleteCar = require("../controllers/deleteCar");
-const createUser = require('../controllers/register')
+const createUser = require("../controllers/register");
+const loginUser = require("../controllers/login");
 
 const router = Router();
 
-router.post('/signup', createUser)
+router.get("/login", loginUser);
+
+router.post("/signup", createUser);
 
 router.get("/", getCars);
 
