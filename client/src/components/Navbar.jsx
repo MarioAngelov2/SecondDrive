b7dda128cc4b5f0b94043f6bd49e2537bcf02a47
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-25 16:05:21 +0300

    Refactor navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import {Container, Nav, Navbar} from 'react-bootstrap'
+import React, { useEffect, useState } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
-import style from '../styles/Navbar.module.css'
+import style from "../styles/Navbar.module.css";
 
 function NavigationBar() {
-  return (
-    <Navbar className='mt-3'>
-      <Container>
-        <Navbar.Brand>SECOND DRIVE</Navbar.Brand>
-        <div className={`${style.navBarText}`}>
-            <Nav.Link href='/'>Начало</Nav.Link>
-            <Nav.Link href='/signup'>Регистрация</Nav.Link>
-            <Nav.Link href='/login'>Вход</Nav.Link>
-        </div>
-      </Container>
-    </Navbar>
-  )
+
+    return (
+        <Navbar sticky="top" expand="md" className="mt-3">
+            <Container>
+                <Navbar.Brand>SECOND DRIVE</Navbar.Brand>
+                <Navbar.Toggle aria-controls="main-navbar" />
+                <Navbar.Collapse id="main-navbar">
+                    <Nav className="ms-auto">
+                        <div className={`${style.navBarText}`}>
+                            <Nav.Link href="/">Начало</Nav.Link>
+                            <Nav.Link href="/signup">Регистрация</Nav.Link>
+                            <Nav.Link href="/login">Вход</Nav.Link>
+                        </div>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
-export default NavigationBar
+export default NavigationBar;
