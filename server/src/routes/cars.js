a61a57008d670aac5cd32e2b56c68a06097caeb0
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-04 09:49:49 +0300

    Add new routes

--- server/src/routes/cars.js
+++ server/src/routes/cars.js
@@ -8,6 +8,9 @@ const deleteCar = require("../controllers/deleteCar");
 const createUser = require("../controllers/register");
 const loginUser = require("../controllers/login");
 const logout = require("../controllers/logout");
+const authenticate = require("../controllers/auth");
+const favorite = require("../controllers/favoriteCar");
+const favoriteCars = require("../controllers/favoriteCars");
 
 const router = Router();
 
@@ -19,7 +22,10 @@ router.post("/signup", createUser);
 router.post("/login", loginUser);
 router.get("/logout", logout);
 
-router.post("/create", upload.single("image"), createCar);
+router.post("/create", authenticate, upload.single("image"), createCar);
+router.put("/favorite", authenticate, favorite);
+router.get("/favoriteCars/ids", favoriteCars);
+router.get("/favoriteCars", )
 router.patch("/update/:id", upload.single("image"), updateCar);
 router.delete("/:id", deleteCar);
 
